11d2d4252cc3a7ff46aaaa3144e525171eeaa889
Talal Salman Zafeer <mustangcreator@gmail.com>
2024-05-02 15:32:44 +0300

    Connecting PostgreSQL

--- server/index.js
+++ server/index.js
@@ -1,12 +1,39 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const pool = require("./db");
 
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
+
+//ROUTES
+
+//create a todo
+
+app.post("/todos", async (req, res) => {
+    try {
+        const { description } = req.body;
+
+        const newTodo = await pool.query(
+            "INSERT INTO todo (description) VALUES($1)"
+            , [description]
+        );
+        res.json(newTodo);
+    } catch (error) {
+        console.log(error.message);    
+    }
+})
+
+//get all todos
+
+//get a todo
+
+//update all todos
+
+//delete a todo
 
 
 app.listen(5000, () => {
